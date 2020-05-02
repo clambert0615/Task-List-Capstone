31675894c7d5f05b7904c91a60322420c0b266dc
Christina Lambert <clambert0615@gmail.com>
2020-05-02 16:02:18 -0400

    Initial commit for task lisk capstone

---
+++ Task List Capstone/Program.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Task_List_Capstone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Task Manager!");
+            Console.WriteLine();
+            List<Task> taskList = Task.GetTaskList();
+           
+            bool askAgain = true;
+            while (askAgain)
+
+            {
+                int menuChoice = GetUserInput("Choose a number. \n \t 1. Lists Tasks \n \t 2. Add Task \n \t 3. Delete task \n \t 4. Mark Task Complete \n \t 5. Edit Task \n \t 6. List Task by Team Member \n \t 7. List Tasks Before a Due Date \n \t 8. Exit Program");
+
+
+                if (menuChoice == 1)
+                {
+                    int taskNumber = 0;
+
+                    for (int i = 0; i < taskList.Count; i++)
+                    {
+                        Console.WriteLine();
+                        taskNumber += 1;
+                        Console.WriteLine($"Task number:  { taskNumber}");
+                        taskList[i].PrintListInfo();
+                        Console.WriteLine();
+                    }
+                }
+                else if (menuChoice == 2)
+                {
+                    Task.AddTask(taskList);
+                    Console.WriteLine();
+                }
+                else if (menuChoice == 3)
+                {
+                    Task.DeleteTask(taskList);
+                }
+                else if (menuChoice == 4)
+                {
+                    Task.MarkComplete(taskList);
+                }
+                else if (menuChoice == 5)
+                {
+                    int taskToEdit = Task.GetTaskToEdit(taskList);
+                    EditTask(taskToEdit, taskList);
+
+                }
+                else if (menuChoice == 6)
+                {
+                    bool getName = true;
+                    while (getName)
+                    {
+                        TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+                        Console.WriteLine("What team member do you want to see tasks for?");
+                        string teamMember = Console.ReadLine();
+                        teamMember = textInfo.ToTitleCase(teamMember);
+                        bool isFound = false; 
+                        
+                       for (int i = 0; i < taskList.Count; i++)
+
+                        {
+                            if (teamMember == taskList[i].TeamMemberName)
+                            {
+
+                                taskList[i].PrintListInfo();
+                                Console.WriteLine();
+                                isFound = true;
+                            }
+                               
+                        }
+                        if (isFound)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Team member not found.  Enter a valid name.");
+
+                        }
+                    }
+                }
+                else if (menuChoice == 7)
+                {
+                    bool getDate = true;
+                    while (getDate)
+                    {
+                        Console.WriteLine("Enter the due date you want to see tasks before");
+                        DateTime userDate = DateTime.Parse(Console.ReadLine());
+                        bool dateFound = false;
+
+                        Console.WriteLine("These are the tasks with due dates before the date you entered:");
+                        for (int i = 0; i < taskList.Count; i++)
+                        {
+                            int result = DateTime.Compare(userDate, taskList[i].DueDate);
+                            if (result > 0)
+                            {
+                                taskList[i].PrintListInfo();
+                                Console.WriteLine();
+                                dateFound = true;
+                            }
+
+                        }
+                        if (dateFound)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("No tasks were found with dates before the date you entered.");
+                        }
+                    }
+                }
+                else if (menuChoice == 8)
+                {
+                    Console.WriteLine("Quitting Task Manager. Goodbye!");
+                    break;
+                }
+            }
+
+        }
+        //methods
+
+        public static int GetUserInput(string message)
+        {
+            while (true)
+            {
+                int input = 0;
+
+                try
+                {
+                    Console.WriteLine(message);
+                    input = int.Parse(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("This is not a number.  Try again");
+                    continue;
+                }
+
+                if (input >= 1 && input <= 8)
+                {
+                    return input;
+
+                }
+
+                else
+                {
+                    Console.WriteLine("This number is not between 1 and 8.  Try again");
+                    continue;
+                }
+
+            }
+        }
+
+        public static void EditTask(int taskToEdit, List<Task> taskList)
+        {
+            bool keepAsking = true;
+            while (keepAsking)
+            {
+                TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+                Console.WriteLine("What would you like to edit?  \n \t To edit team member name enter: name \n \t To edit task description enter: description \n \t To edit due date enter: date \n \t To edit completion status enter: completion");
+                string userEdit = Console.ReadLine().ToLower().Trim();
+                
+
+                if (userEdit == "name" || userEdit == "description" || userEdit == "date" || userEdit == "completion")
+                {
+                    if (userEdit == "name")
+                    {
+                        Console.WriteLine("Who is the new team member responsible for this task?");
+                        string newName = Console.ReadLine();
+                        newName = textInfo.ToTitleCase(newName);
+
+                        taskList[taskToEdit].TeamMemberName = newName;
+                    }
+                    else if (userEdit == "description")
+                    {
+                        Console.WriteLine("What would you like to the new description to be?");
+                        string newDescription = Console.ReadLine().ToLower().Trim();
+                        taskList[taskToEdit].TaskDescription = newDescription;
+                    }
+                    else if (userEdit == "date")
+                    {
+                        Console.WriteLine("What would you like the new due date to be?");
+                        DateTime newDueDate = DateTime.Parse(Console.ReadLine());
+                        taskList[taskToEdit].DueDate = newDueDate;
+                    }
+                    else if (userEdit == "completion")
+                    {
+                        while (true)
+                        {
+                            Console.WriteLine("What would you like the completion status to be? \n \t Enter complete or incomplete");
+                            string completionDecision = Console.ReadLine().ToLower().Trim();
+                            if (completionDecision == "complete")
+                            {
+                                taskList[taskToEdit].Completion = true;
+                                break;
+                            }
+                            else if (completionDecision == "incomplete")
+                            {
+                                taskList[taskToEdit].Completion = false;
+                                break;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Invalid input.  Please try again");
+                                continue;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input.  Please try again.");
+                        continue;
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Try again");
+                    continue;
+                }
+                keepAsking = KeepEditing();
+            }
+
+        }
+        public static bool KeepEditing()
+        {
+            while (true)
+            {
+                Console.WriteLine("Would you like to make another edit for this task? y or n");
+                string continueDecision = Console.ReadLine().ToLower().Trim();
+                if (continueDecision == "y")
+                {
+                    return true;
+                }
+                else if (continueDecision == "n")
+                {
+                    Console.WriteLine("Done Editing");
+                    return false;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+
+        
+
+    }
+}
+
---
+++ Task List Capstone/Task.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Task_List_Capstone
+{
+    class Task
+    {
+        //fields
+
+        private string teamMemberName;
+        private string taskDescription;
+        private DateTime dueDate;
+        private bool completion;
+
+        // properties
+        public string TeamMemberName
+        {
+            get { return teamMemberName; }
+            set { teamMemberName = value; }
+        }
+
+        public string TaskDescription
+        {
+            get { return taskDescription; }
+            set { taskDescription = value; }
+
+        }
+
+        public DateTime DueDate
+        {
+            get { return dueDate; }
+            set { dueDate = value; }
+        }
+
+        public bool Completion
+        {
+            get { return completion; }
+            set { completion = value; }
+        }
+
+        // constructors
+        public Task()
+        {
+            completion = false;
+        }
+
+        public Task(string _teamMemberName, string _taskdescription, DateTime _dueDate)
+        {
+            teamMemberName = _teamMemberName;
+            taskDescription = _taskdescription;
+            dueDate = _dueDate;
+            completion = false;
+        }
+
+        //methods
+
+        public static List<Task> GetTaskList()
+        {
+            List<Task> taskList = new List<Task>
+            {
+                new Task("Adam Smith", "Update Student Workbooks", DateTime.Parse("4/15/2020")),
+                new Task("Jill Jones", "Make Chapter 7 Test", DateTime.Parse("5/01/2020")),
+                new Task("Sara White", "Make Final Exam", DateTime.Parse("5/07/2020")),
+                new Task("Pete Black", "Grade Chapter 7 Test", DateTime.Parse("5/06/2020")),
+                new Task("Adam Smith", "Grade Final Exam", DateTime.Parse("5/23/2020")),
+                new Task("Jill Jones", "Compute Final Grade", DateTime.Parse("5/28/2020"))
+            };
+
+            return taskList;
+
+        }
+
+        public void PrintListInfo()
+        {
+            string completionText;
+
+            if (completion)
+            {
+                completionText = "Yes";
+            }
+            else
+            {
+                completionText = "No";
+            }
+
+            string taskDue = dueDate.ToString("d");
+
+            Console.WriteLine($"\t Team Member Name: {teamMemberName} \n \t Task Description: {taskDescription} \n \t Due Date: {taskDue} \n \t Completion Status: {completionText}");
+        }
+
+        public static void AddTask(List<Task> taskList)
+        {
+            DateTime taskDate = DateTime.Now;
+
+            while (true)
+            {
+                TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+                Console.WriteLine("Who is the team member responsible for this task?");
+                string member = Console.ReadLine();
+                member = textInfo.ToTitleCase(member);
+
+                if (string.IsNullOrEmpty(member))
+                {
+                    Console.WriteLine("Invalid input try again");
+                    continue;
+                }
+                
+                Console.WriteLine("What is the task description?");
+                string description = Console.ReadLine().ToLower().Trim();
+                if (string.IsNullOrEmpty(description))
+                {
+                    Console.WriteLine("Invalid input try again.");
+                    continue;
+                }
+                try
+                {
+                    Console.WriteLine("What is the due date?");
+                    taskDate = DateTime.Parse(Console.ReadLine());
+                }
+                catch(FormatException)
+                {
+                    Console.WriteLine("Invalid date try agian.");
+                    continue;
+                }
+
+
+                if(taskDate == DateTime.MinValue)
+                {
+                    Console.WriteLine("Invalid input try again");
+                    continue;
+                }
+                
+                    taskList.Add(new Task(member, description, taskDate));
+                    break;
+                
+            }
+        }
+
+        public static void DeleteTask(List<Task> taskList)
+        {
+            while (true)
+            {
+                int numToDelete = 0;
+                int taskToDelete;
+
+                try
+                {
+                    Console.WriteLine("Which task would you like to delete?");
+                    numToDelete = int.Parse(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("This is not a number. Try again");
+                    continue;
+                }
+                if (numToDelete > 0 && numToDelete <= taskList.Count)
+                {
+                    taskToDelete = numToDelete - 1;
+                    Task task = taskList[taskToDelete];
+                    string completionText;
+
+                    if (task.completion)
+                    {
+                        completionText = "Yes";
+                    }
+                    else
+                    {
+                        completionText = "No";
+                    }
+
+                    string taskDue = task.dueDate.ToString("d");
+                    Console.WriteLine($"Are you sure you want to delete the following task? y or n \n \t Team Member Name: {task.teamMemberName} \n \t Task Description: {task.taskDescription} \n \t Due Date: {taskDue} \n \t Completion: {completionText} ");
+                    string deleteDecision = Console.ReadLine().ToLower().Trim();
+                    if (deleteDecision == "y")
+                    {
+                        taskList.RemoveAt(numToDelete - 1);
+                        break;
+                    }
+                    else if (deleteDecision == "n")
+                    {
+                        Console.WriteLine("Returning to main menu");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input. Try again");
+                        continue;
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("The number is not in the range.  Try again");
+                    continue;
+                }
+            }
+        }
+
+        public static int GetTaskNumber(List <Task> taskList)
+        {
+            int numToMark;
+            int indexToMark;
+
+            while (true)
+            {
+                try
+                { 
+                Console.WriteLine("What task number do you want to mark as complete?");
+                    numToMark = int.Parse(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("This is not a number. Try again");
+                    continue;
+                }
+                if (numToMark > 0 && numToMark <= taskList.Count)
+                {
+                    indexToMark = numToMark - 1;
+                    return indexToMark;
+                }
+                else
+                {
+                    Console.WriteLine("Number is not in range. Try again");
+                    continue;
+                }
+            }
+        }
+
+        public static void MarkComplete(List<Task> taskList)
+        {
+
+            
+            int taskToMarkComplete = Task.GetTaskNumber(taskList);
+
+            while (true)
+            {
+                string markDecision = Task.VerifyCompletion(taskList, taskToMarkComplete);
+
+                Task task = taskList[taskToMarkComplete];
+                if (markDecision == "y")
+                {
+                    task.completion = true;
+                    break;
+                }
+                else if (markDecision == "n")
+                {
+                    Console.WriteLine("Returning to main menu");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Try again");
+                    continue;
+                }
+            }
+        }
+
+        public static string VerifyCompletion(List<Task>taskList, int taskToMarkComplete)
+        {
+            Task task = taskList[taskToMarkComplete];
+            string completionText;
+
+            if (task.completion)
+            {
+                completionText = "Yes";
+            }
+            else
+            {
+                completionText = "No";
+            }
+
+            string taskDue = task.dueDate.ToString("d");
+            Console.WriteLine($"Are you sure you want to mark the following task as completed? y or n \n \t Team Member Name: {task.teamMemberName} \n \t Task Description: {task.taskDescription} \n \t Due Date: {taskDue} \n \t Completion: {completionText} ");
+            string markDecision = Console.ReadLine().ToLower().Trim();
+            return markDecision;
+        }
+        
+        public static int  GetTaskToEdit(List<Task> taskList)
+        {
+            int numToEdit;
+            int indexToEdit;
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("What task number do you want to edit?");
+                    numToEdit = int.Parse(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("This is not a number. Try again");
+                    continue;
+                }
+                if (numToEdit > 0 && numToEdit <= taskList.Count)
+                {
+                    indexToEdit= numToEdit - 1;
+                    return indexToEdit;
+                }
+                else
+                {
+                    Console.WriteLine("Number is not in range. Try again");
+                    continue;
+                }
+            }
+        }
+    }
+        
+    
+}
